0cc51d24d840166e12b3dab34c5c23ae8bc831a6
Ola <olawunmi263@yahoo.com>
2020-10-02 04:37:49 +0100

    Glue updated with hook

--- src/test/java/com/zoopla/runner/TestRunner.java
+++ src/test/java/com/zoopla/runner/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {"src/test/java/com/zoopla/features"},
         plugin = {"pretty","json:target/report.json","de.monochromata.cucumber.report.PrettyReports:target/pretty-cucumber"},
-        glue = {"com/zoopla/stepDefinitions"}
+        glue = {"com/zoopla/hook","com/zoopla/stepDefinitions"}
 )
 
 
